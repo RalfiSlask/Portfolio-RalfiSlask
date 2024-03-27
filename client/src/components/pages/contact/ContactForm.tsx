5ee683ef15d339a74262d1ae03969944ca968adc
RalfiSlask <nilssonnarki@gmail.com>
2024-03-27 19:45:23 +0100

    feat: add contact form

---
+++ client/src/components/pages/contact/ContactForm.tsx
@@ -0,0 +1,177 @@
+import ArrowLeft from '../../../assets/icons/arrowleft.svg';
+import { FormEvent, useEffect, useState } from 'react';
+import axios from 'axios';
+import { motion, Variants } from 'framer-motion';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+interface IFormInfo {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const ContactForm = () => {
+  const inputVariant: Variants = {
+    offscreen: {
+      y: 200,
+      opacity: 0,
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      rotate: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0,
+        duration: 0.8,
+      },
+    },
+  };
+
+  const buttonVariant: Variants = {
+    offscreen: {
+      x: 200,
+      opacity: 0,
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      rotate: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0,
+        duration: 0.8,
+      },
+    },
+  };
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [formInfo, setFormInfo] = useState<IFormInfo>({ name: '', email: '', message: '' });
+  const [messageSuccessfull, setMessageSuccessFull] = useState('');
+  const [emailNotValidText, setEmailNotValidText] = useState('');
+  const [nameMessageNotValidText, setMessageNotValidText] = useState('');
+  const [nameNotValidText, setNameNotValidText] = useState('');
+
+  const handleChangeOnNameInput = (e: FormEvent<HTMLInputElement>, key: keyof IFormInfo) => {
+    const target = e.target as HTMLInputElement;
+    setFormInfo(prev => ({ ...prev, [key]: target.value }));
+    setMessageSuccessFull('');
+    setEmailNotValidText('');
+    setMessageNotValidText('');
+    setNameNotValidText('');
+  };
+
+  const handleChangeOnTextArea = (e: FormEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    setFormInfo(prev => ({ ...prev, message: target.value }));
+    setMessageSuccessFull('');
+    setEmailNotValidText('');
+    setMessageNotValidText('');
+    setNameNotValidText('');
+  };
+
+  useEffect(() => {
+    console.log(formInfo);
+  }, [formInfo]);
+
+  const submitForm = async (e: FormEvent) => {
+    setFormInfo({ name: '', email: '', message: '' });
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/sendmail', {
+        mailInfo: {
+          name: formInfo.name,
+          email: formInfo.email,
+          message: formInfo.message,
+        },
+      });
+
+      const data = response.data;
+      if (data) {
+        setMessageSuccessFull('Message was sent!');
+      }
+    } catch (err) {
+      console.log(err, 'error sending inputs');
+    }
+  };
+
+  return (
+    <form className="flex flex-col gap-10 w-[400px] sm:w-[500px] md:w-[600px]" onSubmit={submitForm}>
+      <motion.div initial="offscreen" whileInView={'onscreen'} viewport={{ once: true }} transition={{ duration: 0.5 }}>
+        <motion.div className="card" variants={inputVariant}>
+          <input
+            onChange={e => {
+              handleChangeOnNameInput(e, 'name');
+            }}
+            className="contact-input"
+            type="text"
+            placeholder="Name"
+            value={formInfo.name}
+          />
+        </motion.div>
+      </motion.div>
+      <motion.div initial="offscreen" whileInView={'onscreen'} viewport={{ once: true }} transition={{ duration: 0.5 }}>
+        <motion.div className="card" variants={inputVariant}>
+          <input
+            onChange={e => {
+              handleChangeOnNameInput(e, 'email');
+            }}
+            className="contact-input"
+            type="text"
+            placeholder="Email"
+            value={formInfo.email}
+          />
+        </motion.div>
+      </motion.div>
+      <motion.div initial="offscreen" whileInView={'onscreen'} viewport={{ once: true }} transition={{ duration: 0.5 }}>
+        <motion.div className="card" variants={inputVariant}>
+          <textarea
+            onChange={e => {
+              handleChangeOnTextArea(e);
+            }}
+            className="shadow-shadow-input rounded-[12px] w-full h-[260px] outline-none p-6 bg-secondaryBG"
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+            placeholder="Message"
+            style={{ resize: 'none' }}
+            value={formInfo.message}
+          ></textarea>
+        </motion.div>
+      </motion.div>
+
+      <motion.div initial="offscreen" whileInView={'onscreen'} viewport={{ once: true }} transition={{ duration: 0.5 }}>
+        <motion.div className="card" variants={buttonVariant}>
+          <div className="flex justify-between items-center">
+            <button
+              className={`shadow-shadow-icon py-4 text-[1.25rem] h-[60px] rounded-xl text-left transition-gap duration-300 w-[300px] pl-6 relative overflow-hidden uppercase flex items-center 
+            `}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <p className="absolute z-10">Send Message</p>
+              <img
+                src={ArrowLeft}
+                alt="arrow"
+                width="30"
+                height="30"
+                className={`absolute z-10 transition-right duration-500 ${
+                  isHovered ? 'right-[24px]' : 'right-[40px]'
+                } right-8"`}
+              />
+              <span
+                className={`absolute top-0 left-0 bg-blueColor bg-opacity-75 h-full ${
+                  isHovered ? 'w-full transition-all duration-500' : 'w-0 transition-all duration-500'
+                }`}
+              ></span>
+            </button>
+            <p className="text-[1.25rem] text-blueColor">{messageSuccessfull}</p>
+          </div>
+        </motion.div>
+      </motion.div>
+    </form>
+  );
+};
+
+export default ContactForm;
