b88fefc9fafb165f57367c5ed8d3eac5ee73829d
RalfiSlask <nilssonnarki@gmail.com>
2024-03-27 19:43:47 +0100

    feat: add intro heading with both my name and a stylish developer

---
+++ client/src/components/pages/landing/IntroHeading.tsx
@@ -0,0 +1,12 @@
+const IntroHeading = () => {
+  return (
+    <div className="w-full flex justify-center items-center relative">
+      <h1 className="text text-[1rem] sm:text-[1.5rem] md:text-[2rem] font-bold border-r border-solid border-gray-600 pr-4">
+        Matthias Nilsson
+      </h1>
+      <h2 className="pl-4 font-lg text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] pt-[3px] text-blueColor">Developer</h2>
+    </div>
+  );
+};
+
+export default IntroHeading;
