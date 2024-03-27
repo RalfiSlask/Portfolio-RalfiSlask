b81b31a5b6cfbef043385ec13e766baf83c03430
RalfiSlask <nilssonnarki@gmail.com>
2024-03-27 19:51:10 +0100

    feat: opening a link component

---
+++ client/src/components/pages/projects/OpenProjectIcon.tsx
@@ -0,0 +1,17 @@
+import OpenLogo from '../../../assets/arrowopen.svg';
+
+const OpenProjectIcon: React.FC<{ link: string }> = ({ link }) => {
+  return (
+    <div>
+      <a
+        href={link}
+        target="_blank"
+        className="shadow-shadow-icon open-icon hover:shadow-shadow-input transition-shadow duration-300 w-[40px] h-[40px] rounded-md flex justify-center items-center"
+      >
+        <img src={OpenLogo} alt="download" height="25" width="25" />
+      </a>
+    </div>
+  );
+};
+
+export default OpenProjectIcon;
