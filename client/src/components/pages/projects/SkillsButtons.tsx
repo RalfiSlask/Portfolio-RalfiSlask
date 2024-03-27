4a494b9da23950c66766e07754cd86790d1e5489
RalfiSlask <nilssonnarki@gmail.com>
2024-03-27 19:48:16 +0100

    feat: skills button component for mapping out the skills on each project

---
+++ client/src/components/pages/projects/SkillsButtons.tsx
@@ -0,0 +1,13 @@
+import SkillButton from './SkillButton';
+
+const SkillsButtons: React.FC<{ skills: string[] }> = ({ skills }) => {
+  return (
+    <div className="flex flex-wrap items-center gap-4">
+      {skills.map((skill, index) => {
+        return <SkillButton key={index} text={skill} />;
+      })}
+    </div>
+  );
+};
+
+export default SkillsButtons;
