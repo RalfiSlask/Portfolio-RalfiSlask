e460816d28564b4f6822ce7d56e8c97508932050
RalfiSlask <nilssonnarki@gmail.com>
2024-03-28 12:54:51 +0100

    feat: add contact intro component

---
+++ client/src/components/pages/contact/ui/ContactIntro.tsx
@@ -0,0 +1,18 @@
+const ContactIntro = () => {
+  return (
+    <>
+      <h2 className="text-[4rem]">
+        Contact <span className="text-blueColor">.</span>
+      </h2>
+      <div className="flex">
+        <p className="text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] max-w-[400px] sm:max-w-[500px] md:max-w-[600px] leading-10">
+          Have a project in mind or just want to chat about software development? Feel free to reach out below. Your
+          message is <span className="text-blueColor">important to me</span>, and I'll make sure to get back to you as
+          soon as possible.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ContactIntro;
