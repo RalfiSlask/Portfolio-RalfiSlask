5c421d1a4d35c09f7f4e876a7396ac304f54c1f2
RalfiSlask <nilssonnarki@gmail.com>
2024-03-28 12:49:56 +0100

    feat: add audiophile container with astropos

---
+++ client/src/components/pages/projects/AudiophileContainer.tsx
@@ -0,0 +1,22 @@
+import AudiophileImg from '../../../assets/images/audiophile.jpg';
+import Astropos from 'atropos/react';
+
+const AudiophileContainer = () => {
+  return (
+    <Astropos className="atropos w-full" highlight={true}>
+      <a
+        className="p-4 bg-secondaryBG rounded-[12px] flex flex-col gap-2 w-full"
+        href="https://ralfislask.github.io/Audiophile-Ecommerce/#/"
+        target="_blank"
+      >
+        <div className="bg-primaryBG w-full rounded-[25px] p-4 shadow-shadow-medium cursor-pointer flex flex-col gap-6">
+          <div className="relative  rounded-xl w-full">
+            <img src={AudiophileImg} alt="memory" width="500" height="500" className="w-full object-cover" />
+          </div>
+        </div>
+      </a>
+    </Astropos>
+  );
+};
+
+export default AudiophileContainer;
