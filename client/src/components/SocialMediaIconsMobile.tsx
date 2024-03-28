2099ae13f3ad508f6400f546e75b1c824b242107
RalfiSlask <nilssonnarki@gmail.com>
2024-03-28 17:36:57 +0100

    feat: add specific social media navbar for mobile size

---
+++ client/src/components/SocialMediaIconsMobile.tsx
@@ -0,0 +1,38 @@
+import linkedinLogo from '../assets/linkedin.svg';
+import frontendmentorLogo from '../assets/frontendmentor.svg';
+import githubLogo from '../assets/github.svg';
+import { useEffect, useState } from 'react';
+
+const SocialMediaIconsMobile = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  return (
+    <nav
+      className={` bg-secondaryBG md:bg-primaryBG  h-[60px] fixed left-1/2 -translate-x-1/2 duration-700 z-50 flex items-center gap-4`}
+    >
+      <div className="hover:scale-[115%] transition-scale duration-300">
+        <a href="https://www.linkedin.com/in/matthias-nilsson-68023b294/" target="_blank">
+          <img src={linkedinLogo} alt="linkedin" width="25" height="25" />
+        </a>
+      </div>
+
+      <div className="hover:scale-[115%] transition-scale duration-300 w-[28px] h-[28px]">
+        <a href="https://github.com/RalfiSlask" target="_blank">
+          <img src={githubLogo} alt="github" width="28" height="28" />
+        </a>
+      </div>
+
+      <div className="hover:scale-[115%] transition-scale duration-300">
+        <a href="https://www.frontendmentor.io/profile/RalfiSlask" target="_blank">
+          <img src={frontendmentorLogo} alt="frontendmentor" width="25" height="25" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default SocialMediaIconsMobile;
