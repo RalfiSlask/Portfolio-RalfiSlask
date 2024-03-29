ff8aa18b733bf9762bd3cfc15891d2dd021cd2ac
RalfiSlask <nilssonnarki@gmail.com>
2024-03-29 16:11:15 +0100

    feat: changed dimensions

--- client/src/components/pages/contact/ui/Copyright.tsx
+++ client/src/components/pages/contact/ui/Copyright.tsx
@@ -1,5 +1,9 @@
 const Copyright = () => {
-  return <p className="text-[1rem] md:text-[1.25rem]">&copy; 2024 Matthias. All rights reserved .</p>;
+  return (
+    <p className="bottom-6 xl:bottom-10 absolute left-1/2 -translate-x-1/2 text-[1rem] md:text-[1.25rem]">
+      &copy; 2024 Matthias. All rights reserved .
+    </p>
+  );
 };
 
 export default Copyright;
