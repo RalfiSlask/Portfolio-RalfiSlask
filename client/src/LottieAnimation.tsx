396d3c032c7a292e1cbfc2b3f74e4625c3e6b01b
RalfiSlask <nilssonnarki@gmail.com>
2024-03-29 12:40:20 +0100

    feat: change to grid

--- client/src/LottieAnimation.tsx
+++ client/src/LottieAnimation.tsx
@@ -9,7 +9,11 @@ const LottieAnimation = () => {
 
   const { View } = useLottie(options);
 
-  return <div className="w-[400px] opacity-90">{View}</div>;
+  return (
+    <div className="col-span-4 xl:col-span-4 md:col-span-8 w-full xl:max-w-[400px] xl:max-h-[400px] h-full opacity-90">
+      {View}
+    </div>
+  );
 };
 
 export default LottieAnimation;
