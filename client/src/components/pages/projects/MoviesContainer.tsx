b0e59da6ff8e8baa5cd1928c9d86e2b59b402063
RalfiSlask <nilssonnarki@gmail.com>
2024-03-29 16:12:24 +0100

    feat: changed dimensions

--- client/src/components/pages/projects/MoviesContainer.tsx
+++ client/src/components/pages/projects/MoviesContainer.tsx
@@ -31,35 +31,37 @@ const MoviesContainer = () => {
   };
 
   return (
-    <motion.div
-      initial="offscreen"
-      whileInView={'onscreen'}
+    <a
+      href="https://ralfislask.github.io/Entertainment-Web-App/"
+      target="_blank"
       className="w-full col-span-4 md:col-start-2 md:col-end-5 xl:col-span-4 "
-      viewport={{ once: true }}
-      transition={{ duration: 0.5 }}
     >
-      <motion.div className="card" variants={projectVariant}>
-        <div className="bg-primaryBG p-10 shadow-shadow-medium max-h-[700px] xl:max-h-[450px] group rounded-[25px] cursor-pointer flex flex-col gap-6">
-          <div className="relative overflow-hidden rounded-xl min-h-[180px]">
-            <img
-              src={MoviesImg}
-              alt="memory"
-              width="500"
-              height="500"
-              className="transition-transform duration-300 object-cover w-full h-full transform group-hover:scale-[110%] group:hover-opacity"
-            />
-          </div>
-          <ProjectInfoContainer projectInfo={projectInfo} />
-          <div className="flex justify-between items-center">
-            <SkillsButtons skills={skills} />
-            <div className="flex items-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <GithubIcon link="https://github.com/RalfiSlask/Entertainment-Web-App?tab=readme-ov-file" />
-              <OpenProjectIcon link="https://ralfislask.github.io/Entertainment-Web-App/" />
+      <motion.div
+        initial="offscreen"
+        whileInView={'onscreen'}
+        className="w-full"
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.div className="card" variants={projectVariant}>
+          <div className="bg-primaryBG p-10 shadow-shadow-medium md:max-h-[500px] max-h-[700px] xl:max-h-[450px] group rounded-[25px] cursor-pointer flex flex-col gap-6">
+            <div className="relative overflow-hidden rounded-xl min-h-[180px]">
+              <img
+                src={MoviesImg}
+                alt="memory"
+                width="500"
+                height="500"
+                className="transition-transform duration-300 object-cover w-full h-full transform group-hover:scale-[110%] group:hover-opacity"
+              />
+            </div>
+            <ProjectInfoContainer projectInfo={projectInfo} />
+            <div className="flex justify-between items-center">
+              <SkillsButtons skills={skills} />
             </div>
           </div>
-        </div>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </a>
   );
 };
 
