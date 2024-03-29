6b12483c047742daff5bb6b508ea7dd0bc4202b2
RalfiSlask <nilssonnarki@gmail.com>
2024-03-29 12:41:27 +0100

    feat: change dimensions

--- client/src/components/pages/landing/IntroHeading.tsx
+++ client/src/components/pages/landing/IntroHeading.tsx
@@ -1,6 +1,6 @@
 const IntroHeading = () => {
   return (
-    <div className="w-full flex justify-center items-center relative">
+    <div className="w-full  flex justify-center items-center relative">
       <h1 className="text text-[1rem] sm:text-[1.5rem] md:text-[2rem] font-bold border-r border-solid border-gray-600 pr-4">
         Matthias Nilsson
       </h1>
