04eadea7bbdd6c8c8769aa7cfa4bcc5d58fcdfe4
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 18:51:34 +0100

    feat: create description for skills section

---
+++ client/src/components/pages/skills/ui/SkillsDescription.tsx
@@ -0,0 +1,11 @@
+const SkillsDescription = () => {
+  return (
+    <p className="sm:text-[1rem] md:text-[1.5rem] sm:max-w-[500px] md:max-w-[700px] xl:max-w-[600px] leading-10">
+      Always trying to <span className="text-blueColor">add</span> to my repertoire of technolgies, either if it is
+      testing new <span className="text-blueColor">AI tools</span> or studying new{' '}
+      <span className="text-blueColor">frameworks</span> .
+    </p>
+  );
+};
+
+export default SkillsDescription;
