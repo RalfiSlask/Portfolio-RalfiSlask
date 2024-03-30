1fadec0d70b30c8a24edd24b21e29c9f3db17939
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 09:57:37 +0100

    feat: change dimensions

--- client/src/components/pages/contact/ui/Copyright.tsx
+++ client/src/components/pages/contact/ui/Copyright.tsx
@@ -1,6 +1,6 @@
 const Copyright = () => {
   return (
-    <p className="bottom-6 xl:bottom-10 absolute left-1/2 -translate-x-1/2 text-[1rem] md:text-[1.25rem]">
+    <p className="bottom-6 xl:bottom-10 absolute left-1/2 -translate-x-1/2 text-[1rem] md:text-[1.25rem] text-center w-full">
       &copy; 2024 Matthias. All rights reserved .
     </p>
   );
