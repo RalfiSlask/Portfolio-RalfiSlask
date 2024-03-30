081957ec01cdbc158949207678b5acc98deed46c
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 16:42:13 +0100

    feat: create new file with framer motion variants

---
+++ client/src/utils/motionvariants.tsx
@@ -0,0 +1,110 @@
+import { Variants } from 'framer-motion';
+
+export const chatSpotifyVariant: Variants = {
+  offscreen: {
+    y: 100,
+    opacity: 0,
+  },
+  onscreen: {
+    y: 25,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0,
+      duration: 0.8,
+    },
+  },
+};
+
+export const LargeTextVariant: Variants = {
+  offscreen: {
+    x: -50,
+    opacity: 0,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      type: 'ease',
+      bounce: 0,
+      duration: 0.4,
+    },
+  },
+};
+
+export const MediumTextVariant: Variants = {
+  offscreen: {
+    x: -50,
+    opacity: 0,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+
+    transition: {
+      type: 'ease',
+      bounce: 0,
+      duration: 0.4,
+      delay: 0.2,
+    },
+  },
+};
+
+export const FadeInFromRight: Variants = {
+  offscreen: {
+    x: 50,
+    opacity: 0,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+
+    transition: {
+      type: 'ease',
+      bounce: 0,
+      duration: 0.6,
+    },
+  },
+};
+
+export const FadeInFromRightDelayed: Variants = {
+  offscreen: {
+    x: 50,
+    opacity: 0,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+
+    transition: {
+      type: 'ease',
+      bounce: 0,
+      duration: 0.6,
+      delay: 0,
+    },
+  },
+};
+
+export const FadeInFromRightLast: Variants = {
+  offscreen: {
+    x: 50,
+    opacity: 0,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+
+    transition: {
+      type: 'ease',
+      bounce: 0,
+      duration: 0.6,
+      delay: 0.2,
+    },
+  },
+};
