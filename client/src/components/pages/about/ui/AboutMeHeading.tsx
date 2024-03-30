b0d0e319d911e4644381b7c8c1590ec5d103d70c
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 16:41:55 +0100

    feat: create about me heading

---
+++ client/src/components/pages/about/ui/AboutMeHeading.tsx
@@ -0,0 +1,9 @@
+const AboutMeHeading = () => {
+  return (
+    <h2 className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text-[4rem] font-medium">
+      About Me <span className="text-blueColor">.</span>
+    </h2>
+  );
+};
+
+export default AboutMeHeading;
