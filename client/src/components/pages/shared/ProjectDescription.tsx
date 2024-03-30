f2c1bb321a9911037906fab801be7e09bce88c29
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 16:42:35 +0100

    feat: create about me description component

---
+++ client/src/components/pages/shared/ProjectDescription.tsx
@@ -0,0 +1,13 @@
+const ProjectDescription = () => {
+  return (
+    <p className="text-[1rem] sm:text-[1.25rem] md:text-[1.35rem] leading-14 tracking-wide max-w-[600px]">
+      Within my Github Profile, I maintain a collection of <span className="text-blueColor">60+ repositories </span>
+      showcasing a commitment to continuous learning. This assortment includes both{' '}
+      <span className="text-blueColor">frontend</span> and <span className="text-blueColor">fullstack </span>
+      projects, reflecting my independent endeavors and excersises completed during my academic studies
+      <span className="text-blueColor">.</span>
+    </p>
+  );
+};
+
+export default ProjectDescription;
