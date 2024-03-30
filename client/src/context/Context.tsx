f61f6a7a71404dc366cb9ee9817a74bee506ad9e
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 16:40:42 +0100

    feat: add context

---
+++ client/src/context/Context.tsx
@@ -0,0 +1,38 @@
+import { createContext, ReactNode, useState, useEffect } from 'react';
+
+type ContextProps = {
+  isScreenSizeMobile: boolean;
+};
+
+export const Context = createContext<ContextProps | undefined>(undefined);
+
+type ChildProps = {
+  children: ReactNode;
+};
+
+export const ContextProvider: React.FC<ChildProps> = ({ children }) => {
+  const [isScreenSizeMobile, setIsScreenSizeMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 768) {
+        setIsScreenSizeMobile(true);
+      } else {
+        setIsScreenSizeMobile(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.addEventListener('resize', handleResize);
+  }, []);
+
+  const contextValue = {
+    isScreenSizeMobile: isScreenSizeMobile,
+  };
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
