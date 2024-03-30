cd9e921cb5504ecaef8b237720cf6f65402c7d22
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 18:51:21 +0100

    feat: create heading for skills section

---
+++ client/src/components/pages/skills/ui/SkillsHeading.tsx
@@ -0,0 +1,9 @@
+const SkillsHeading = () => {
+  return (
+    <h2 className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text-[4rem] font-medium">
+      Technical Skills <span className="text-blueColor">.</span>
+    </h2>
+  );
+};
+
+export default SkillsHeading;
