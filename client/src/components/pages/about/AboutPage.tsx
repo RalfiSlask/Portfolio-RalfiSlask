3ad6efcbcc8e4bba5a41970faa085e4c9d34d5b0
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 16:41:41 +0100

    feat: use isScreenMobile from context and render effects conditionally, also move variants to its own file

--- client/src/components/pages/about/AboutPage.tsx
+++ client/src/components/pages/about/AboutPage.tsx
@@ -1,60 +1,18 @@
+import { useContext } from 'react';
 import Chat from './Chat';
 import SpotifySection from './SpotifySection';
-import { motion, Variants } from 'framer-motion';
+import { motion } from 'framer-motion';
+import AboutMeHeading from './ui/AboutMeHeading';
+import AboutMeDescription from './ui/AboutMeDescription';
+import { chatSpotifyVariant, LargeTextVariant, MediumTextVariant } from '../../../utils/motionvariants';
+import { Context } from '../../../context/Context';
 
 const AboutPage = () => {
-  const chatSpotifyVariant: Variants = {
-    offscreen: {
-      y: 100,
-      opacity: 0,
-    },
-    onscreen: {
-      y: 25,
-      opacity: 1,
-      rotate: 0,
-      transition: {
-        type: 'spring',
-        bounce: 0,
-        duration: 0.8,
-      },
-    },
-  };
+  const context = useContext(Context);
 
-  const LargeTextVariant: Variants = {
-    offscreen: {
-      x: -50,
-      opacity: 0,
-    },
-    onscreen: {
-      x: 0,
-      opacity: 1,
-      rotate: 0,
-      transition: {
-        type: 'ease',
-        bounce: 0,
-        duration: 0.4,
-      },
-    },
-  };
+  if (!context) return;
 
-  const MediumTextVariant: Variants = {
-    offscreen: {
-      x: -50,
-      opacity: 0,
-    },
-    onscreen: {
-      x: 0,
-      opacity: 1,
-      rotate: 0,
-
-      transition: {
-        type: 'ease',
-        bounce: 0,
-        duration: 0.4,
-        delay: 0.2,
-      },
-    },
-  };
+  const { isScreenSizeMobile } = context;
 
   return (
     <div className="grid-fill-4812 px-6 flex flex-col gap-20 w-full relative items-center">
@@ -67,22 +25,20 @@ const AboutPage = () => {
           className="col-span-4 md:col-start-2 md:col-end-8 xl:col-span-12"
         >
           <div className="flex flex-col gap-10 w-full">
-            <motion.div variants={LargeTextVariant}>
-              <h2 className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text-[4rem] font-medium">
-                About Me <span className="text-blueColor">.</span>
-              </h2>
-            </motion.div>
-            <motion.div variants={MediumTextVariant}>
-              <div className="flex justify-between">
-                <p className="text-[1rem] sm:text-[1.25rem] max-w-[800px] leading-10">
-                  Experience the heart of my portfolio by engaging in{' '}
-                  <span className="text-blueColor">conversation</span> with my replica or by
-                  <span className="text-blueColor"> connecting </span>
-                  through music. Lets embark on this journey together
-                  <span className="text-blueColor"> .</span>
-                </p>
-              </div>
-            </motion.div>
+            {isScreenSizeMobile ? (
+              <AboutMeHeading />
+            ) : (
+              <motion.div variants={LargeTextVariant}>
+                <AboutMeHeading />
+              </motion.div>
+            )}
+            {isScreenSizeMobile ? (
+              <AboutMeDescription />
+            ) : (
+              <motion.div variants={MediumTextVariant}>
+                <AboutMeDescription />
+              </motion.div>
+            )}
           </div>
         </motion.div>
 
