837bbe16094ba1e2e5ba82d2a48f6f2b2d0d8f24
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 09:55:58 +0100

    feat: change dimensions

--- client/src/components/pages/contact/ui/BuiltWith.tsx
+++ client/src/components/pages/contact/ui/BuiltWith.tsx
@@ -1,6 +1,6 @@
 const BuiltWith = () => {
   return (
-    <div className="absolute right-1/2 translate-x-1/2 bottom-12 xl:bottom-10 md:bottom-16 xl:right-[10%] xl:translate-x-1/2 z-20 w-[400px] xl:w-[300px] h-20 opacity-60">
+    <div className="absolute right-1/2 translate-x-1/2 bottom-24 xl:bottom-10 md:bottom-16 xl:right-[10%] xl:translate-x-1/2 z-20 w-[300px] sm:w-[400px] xl:w-[300px] h-20 opacity-60">
       <div>
         Page built with <span className="text-blueColor">React</span> &{' '}
         <span className="text-blueColor">Tailwind,</span> deployed with <span className="text-blueColor">Vite</span>.
