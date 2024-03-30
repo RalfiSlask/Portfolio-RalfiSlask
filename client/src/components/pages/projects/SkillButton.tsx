62bdd637a832c5c57b9a7e4550974c931c4bad73
RalfiSlask <nilssonnarki@gmail.com>
2024-03-30 09:56:24 +0100

    feat: change styling

--- client/src/components/pages/projects/SkillButton.tsx
+++ client/src/components/pages/projects/SkillButton.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
 const SkillButton: React.FC<{ text: string }> = ({ text }) => {
-  return <button className="shadow-shadow-oval px-2 py-1 rounded-full bg-secondaryBG text-blueColor">{text}</button>;
+  return (
+    <button className="shadow-shadow-icon px-4 py-2 text-[0.85rem]  rounded-full text-blueColor font-medium">
+      {text}
+    </button>
+  );
 };
 
 export default SkillButton;
