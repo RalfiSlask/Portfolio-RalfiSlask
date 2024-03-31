01077484197d242cfdff3219245c391830fbf9da
RalfiSlask <nilssonnarki@gmail.com>
2024-03-31 09:19:29 +0200

    feat: add typing indicator component

---
+++ client/src/components/pages/about/ui/TypingIndicator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TypingIndicator = () => {
+  return (
+    <div className="typing-indicator ml-12">
+      <span className="typing-indicator-dot"></span>
+      <span className="typing-indicator-dot"></span>
+      <span className="typing-indicator-dot"></span>
+    </div>
+  );
+};
+
+export default TypingIndicator;
