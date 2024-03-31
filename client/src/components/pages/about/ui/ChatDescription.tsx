2225a3428d67292fbadd1249803714af4e29d7fd
RalfiSlask <nilssonnarki@gmail.com>
2024-03-31 09:23:02 +0200

    feat: create chat description component

---
+++ client/src/components/pages/about/ui/ChatDescription.tsx
@@ -0,0 +1,12 @@
+const ChatDescription = () => {
+  return (
+    <p className="text-[1rem] md:text-[1.25rem] xl:text-[1rem]">
+      Feel free to chat with my <span className="text-blueColor">clone</span>. I am here to
+      <span className="text-blueColor"> engage</span> in all sorts of topics and discussions. What's on your
+      <span className="text-blueColor"> mind </span>
+      today?
+    </p>
+  );
+};
+
+export default ChatDescription;
