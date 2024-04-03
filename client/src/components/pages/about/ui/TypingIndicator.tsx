3f23536a8a4eb213ff4f607c0990940e3370fbf5
RalfiSlask <nilssonnarki@gmail.com>
2024-04-03 20:07:13 +0200

    feat: remove left margin on mobile

--- client/src/components/pages/about/ui/TypingIndicator.tsx
+++ client/src/components/pages/about/ui/TypingIndicator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TypingIndicator = () => {
   return (
-    <div className="typing-indicator ml-12">
+    <div className="typing-indicator ml-0 sm:ml-12">
       <span className="typing-indicator-dot"></span>
       <span className="typing-indicator-dot"></span>
       <span className="typing-indicator-dot"></span>
