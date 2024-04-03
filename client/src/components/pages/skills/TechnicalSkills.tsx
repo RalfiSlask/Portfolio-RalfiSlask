3a0d3e7770fb30d9dbf8d4c25fbc6bf7fefb8023
RalfiSlask <nilssonnarki@gmail.com>
2024-04-03 20:03:33 +0200

    feat: change dimensions of technical skills circles for mobile

--- client/src/components/pages/skills/TechnicalSkills.tsx
+++ client/src/components/pages/skills/TechnicalSkills.tsx
@@ -46,7 +46,7 @@ const TechnicalSkills = () => {
         <div className="col-span-4 md:col-span-8 xl:col-span-7 flex justify-center relative items-center h-[700px] md:h-[650px] max-h-[500px] sm:max-w-[500px] md:max-w-[700px] w-full">
           <div className="w-full h-full">
             <article className="flex justify-center relative items-center gap-10 h-full max-h-[500px] max-w-[500px] md:max-w-[700px] w-full sm:col-span-4 xl:col-span-7 shadow-shadow-medium p-6 rounded-[25px]">
-              <div className="max-w-[400px] h-[400px] w-full border border-solid border-secondaryBG rounded-full relative">
+              <div className="max-w-[300px] h-[300px] sm:max-w-[400px] sm:h-[400px] w-full border border-solid border-secondaryBG rounded-full relative">
                 <div className={`transition-opacity duration-300 ${skillIndex === 0 ? 'opacity-100' : 'opacity-0'}`}>
                   <Frontend />
                 </div>
@@ -59,16 +59,34 @@ const TechnicalSkills = () => {
               </div>
 
               <div
-                className={`w-[200px] h-[200px] flex justify-center items-center absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 rounded-full shadow-shadow-skills`}
+                className={`w-[125px] h-[125px] sm:w-[200px] sm:h-[200px] flex justify-center items-center absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 rounded-full shadow-shadow-skills`}
               >
                 {skillIndex === 0 && (
-                  <h2 className={`${skillIndex === 0 ? 'opacity-100' : ''} text-[1.5rem] text-blueColor`}>Frontend</h2>
+                  <h2
+                    className={`${
+                      skillIndex === 0 ? 'opacity-100' : ''
+                    } text-[1.25rem] sm:text-[1.5rem] text-blueColor`}
+                  >
+                    Frontend
+                  </h2>
                 )}
                 {skillIndex === 1 && (
-                  <h2 className={`${skillIndex === 1 ? 'opacity-100' : ''} text-[1.5rem] text-blueColor`}>Backend</h2>
+                  <h2
+                    className={`${
+                      skillIndex === 1 ? 'opacity-100' : ''
+                    } text-[1.25rem] sm:text-[1.5rem] text-blueColor`}
+                  >
+                    Backend
+                  </h2>
                 )}
                 {skillIndex === 2 && (
-                  <h2 className={`${skillIndex === 2 ? 'opacity-100' : ''} text-[1.5rem] text-blueColor`}>Tools</h2>
+                  <h2
+                    className={`${
+                      skillIndex === 2 ? 'opacity-100' : ''
+                    } text-[1.25rem] sm:text-[1.5rem] text-blueColor`}
+                  >
+                    Tools
+                  </h2>
                 )}
               </div>
 
