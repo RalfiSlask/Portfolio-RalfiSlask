0ecd87dac2b085de384fe7d49975dfae26442b49
RalfiSlask <nilssonnarki@gmail.com>
2025-03-02 14:14:32 +0100

    feat: fix sentance

--- client/src/components/pages/skills/ui/SkillsDescription.tsx
+++ client/src/components/pages/skills/ui/SkillsDescription.tsx
@@ -1,7 +1,7 @@
 const SkillsDescription = () => {
   return (
     <p className="text-white sm:text-[1rem] md:text-[1.5rem] sm:max-w-[500px] md:max-w-[700px] xl:max-w-[600px] leading-10">
-      Always trying to <span className="text-blueColor">add</span> to my repertoire of technolgies, either if it is
+      Always trying to <span className="text-blueColor">add</span> new stuff to my repertoire of technolgies, either if it is
       testing new <span className="text-blueColor">AI tools</span> or studying new{' '}
       <span className="text-blueColor">frameworks</span> .
     </p>
